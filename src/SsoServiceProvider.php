3c00bcd11a4458810bf92b62c3b84a42d4fb8640
venespana <venespana159@gmail.com>
2019-05-22 23:11:20 +0200

    loaded routes in project

--- src/SsoServiceProvider.php
+++ src/SsoServiceProvider.php
@@ -23,8 +23,8 @@ class SsoServiceProvider extends ServiceProvider
         //     Module::load();
 
         //     $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
-        //     $this->loadRoutesFrom(__DIR__.'/../routes/api.php');
 
+        $this->loadRoutesFrom(__DIR__.'/../routes/sso.php');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
     }
 
