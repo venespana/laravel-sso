221c6fa7d3ff8bd2588c39190a1d80b5973d7444
venespana <venespana159@gmail.com>
2019-05-22 22:36:00 +0200

    added base migration and configs

---
+++ configs/configs.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'broker_table' => 'brokers'
+];
---
+++ database/migrations/2019_05_22_000000_create_brokers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBrokersTable extends Migration
+{
+    /**
+     * @inheritDoc
+     * @see Illuminate\Database\Migrations\Migration@up
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(config('sso.broker_table', 'brokers'), function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->comment('Broker name');
+            $table->string('hash')->unique()->comment('Broker unique hash identifiactor');
+            $table->string('secret')->comment('secret key to make hand shake');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(config('sso.broker_table', 'brokers'));
+    }
+}
